eb2a019accde008f343235da616a3825766d9336
Student <student@RU-005.local>
2016-01-21 16:07:33 -0700

    bonus complete

--- main.js
+++ main.js
@@ -11,7 +11,29 @@
 
 // console.log(letterCapitalize("this is a string that needs to be capitalized"))
 
-var wordCount = function(string) {
-	var stringArray = string.split(" ")
-	return stringArray.length
+// var wordCount = function(string) {
+// 	var stringArray = string.split(" ")
+// 	return stringArray.length
+// }
+
+
+var primeTime = function(num) {
+	if(num < 2) {
+		return false
+	}
+	else if(num === 2) {
+		return true
+	}
+	else if(num % 2 === 0) {
+		return false
+	}
+	else {
+		for(i = 3; i < (num/2); i++) {
+			if(num % i === 0) {
+				return false
+			}
+		}
+	}
+	return true
 }
+
