2754c4041bcd10f615b608bcf08dc09662f977c8
Student <student@RU-005.local>
2016-01-21 15:48:12 -0700

    #2 done

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-var letterCapitalize = function(string) {
+// var letterCapitalize = function(string) {
+// 	var stringArray = string.split(" ")
+// 	for(word in stringArray) {
+// 		var wordCap = stringArray[word].split("")
+// 		wordCap[0] = wordCap[0].toUpperCase()
+// 		stringArray[word] = wordCap.join("")
+// 	}
+// 	var newString = stringArray.join(" ")
+// 	return newString
+// }
+
+// console.log(letterCapitalize("this is a string that needs to be capitalized"))
+
+var wordCount = function(string) {
 	var stringArray = string.split(" ")
-	for(word in stringArray) {
-		var wordCap = stringArray[word].split("")
-		wordCap[0] = wordCap[0].toUpperCase()
-		stringArray[word] = wordCap.join("")
-	}
-	var newString = stringArray.join(" ")
-	return newString
+	return stringArray.length
 }
-
-console.log(letterCapitalize("this is a string that needs to be capitalized"))
-
